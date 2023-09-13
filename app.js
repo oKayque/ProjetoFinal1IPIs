733bddf17cd1725673d4646631064320cc0c813c
oKayque <rodrigueskayque9@gmail.com>
2023-09-13 17:03:40 -0300

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+var lista_de_produtos = []
+
+
+while(true){
+
+    var opcao = prompt(`
+        Bem vindo ao cadastro de produtos!
+  
+        1. Cadastrar novo produto
+        2. Editar produto
+        3. Remover produto
+        4. Visualizar todos os produtos
+    `)
+
+    if(opcao == 1)  {
+        //codigo para adicionar produto
+    }else if(opcao ==2) {
+        //codigo para editar produto
+    }else if(opcao ==3) {
+        //codigo para remover
+    }else if(opcao == 4) {
+        //codigo para visualizar
+    }else{
+        alert("Nenhuma das opções foi selecionada!")
+    }
+}
